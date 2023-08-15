c66362c7a60fe81d5f25a0c1792952135b5c50ca
Alexis Rabbia <alexisrabbia@gmail.com>
2023-08-15 18:50:33 -0300

    e1 parte 2

---
+++ exercises/E1c.py
@@ -0,0 +1,16 @@
+
+"""
+Calcular la cantidad de unidades a comprar.
+Restricción: Usar el operador de división entera.
+"""
+
+precio = 3.74
+presupuesto_disponible = 10
+cantidad_a_comprar = presupuesto_disponible // precio
+
+print ("la cantidad a comprar es ", cantidad_a_comprar)
+# COMPLETAR - INICIO
+
+# COMPLETAR - FIN
+
+assert cantidad_a_comprar == 2
