77286269d76ea5fecf322d01661bacbf2ee9cb77
Alexis Rabbia <alexisrabbia@gmail.com>
2023-08-16 22:14:17 -0300

    mitad de ej 4

---
+++ exercises/E4a.py
@@ -0,0 +1,15 @@
+"""Conversiones Básicas"""
+
+
+"""
+Convertir los numeros de string a enteros y luego sumarlos.
+"""
+
+numero_01 = "123"
+numero_02 = "456"
+numero_03 = "789"
+numero_04 = "132"
+
+suma_de_numeros = int(numero_01)+int(numero_02)+int(numero_03)+int(numero_04) 
+print("La suma de los num es ", suma_de_numeros)
+assert suma_de_numeros == 1500
---
+++ exercises/E4c.py
@@ -0,0 +1,14 @@
+"""
+Convertir los numeros de binario, octal y hexadecimal a enteros y luego
+multiplicarlos.
+"""
+
+numero_binario = "0b111010110101110111101000000"
+numero_octal = "0o1425"
+numero_hexadecimal = "0x6f540"
+
+# COMPLETAR - INICIO
+
+# COMPLETAR - FIN
+
+assert multiplicacion_de_numeros == 44397345600000000
